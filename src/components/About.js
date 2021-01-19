0e9fba839bf1463a29a082094645c0861b601281
Leo <voonlihhaur@gmail.com>
2021-01-19 16:51:04 +0800

    fix a tag no referrer

--- src/components/About.js
+++ src/components/About.js
@@ -4,7 +4,7 @@ const About = () => {
   return (
     <div>
       <h4>Version 1.0.0</h4>
-      <p>forked from <a target="_blank" href='https://www.youtube.com/watch?v=w7ejDZ8SWv8'>Traversy Media</a></p>
+      <p>forked from <a target="_blank" rel="noreferrer"  href='https://www.youtube.com/watch?v=w7ejDZ8SWv8'>Traversy Media</a></p>
       <p>Deploy by Leo Voon</p>
       <Link to='/' style={{ marginTop: '1rem' }}>Go Back</Link>
     </div>
