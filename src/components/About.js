5422332dc4bdbe448960edb4c16b4b88c43158cd
Leo <voonlihhaur@gmail.com>
2021-01-19 16:32:15 +0800

    Add files

--- src/components/About.js
+++ src/components/About.js
@@ -4,6 +4,8 @@ const About = () => {
   return (
     <div>
       <h4>Version 1.0.0</h4>
+      <p>forked from <Link to='https://www.youtube.com/watch?v=w7ejDZ8SWv8'>Traversy Media</Link></p>
+      <p>Deploy by Leo Voon</p>
       <Link to='/'>Go Back</Link>
     </div>
   )
